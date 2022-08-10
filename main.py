e82a0d9fe939e972d9cf15dfdc839d0a8fb60e31
João Victor Lira Victor <109807072+eujoaovlira@users.noreply.github.com>
2022-08-10 14:22:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+def calculadora (num1, num2, op):
+  
+  if op == 1:
+    num1 = float(input("digite um numero"))
+    num2 = float(input("digite o um numero"))
+    print ("Resposta", num1+num2)
+  elif op == 2:
+    num1 = float(input("digite um numero"))
+    num2 = float(input("digite o um numero"))
+    print ("Resposta", num1-num2)
+  elif op == 3:
+    num1 = float(input("digite um numero"))
+    num2 = float(input("digite o um numero"))
+    print ("Resposta", num1*num2)
+  elif op == 4:
+    num1 = float(input("digite um numero"))
+    num2 = float(input("digite o um numero"))
+    print ("Resposta", num1/num2)
+    
+    
+
+
+calculadora (10,30, 1)
+calculadora (20,30, 2)
+calculadora (30,30, 3)
+calculadora (40,30, 4)
